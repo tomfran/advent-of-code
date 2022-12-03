b06c157e7a989bc1dab344251d6b1e792241cdb0
Francesco <tomaselli.fr@gmail.com>
2022-12-03 08:56:52 +0100

    Added third solution

---
+++ src/03_Rucksack_Reorganization.py
@@ -0,0 +1,31 @@
+from utils.api import get_input
+from functools import reduce
+
+input_str = get_input(3)
+
+e = list(map(lambda x: x.strip(), input_str.splitlines()))
+
+
+def priority(s):
+    ref = "a" if s.islower() else "A"
+    ret = ord(s) - ord(ref) + 1
+    if not s.islower():
+        ret += 26
+    return ret
+
+
+ans = 0
+for s in e:
+    n = len(s) // 2
+    el = list(set(s[:n]) & set(s[n:]))[0]
+    ans += priority(el)
+
+print(ans)
+
+ans = 0
+for i in range(0, len(e), 3):
+    el = reduce(lambda a, b: a & b, [set(r) for r in e[i : i + 3]])
+    el = list(el)[0]
+    ans += priority(el)
+
+print(ans)
